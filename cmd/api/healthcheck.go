993d3a038d8cded92c5ff9e56321b5b9bd220bb6
Ricardo Mendes <Ricardo Mendes>
2022-04-30 20:34:49 -0300

    Basic CORS

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -11,7 +11,7 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"version":     version,
 	}
 
-	err := app.writeJSON(w, 202, envelope{"health": data}, nil)
+	err := app.writeJSON(w, 200, envelope{"health": data}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -37,6 +38,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -67,6 +71,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "a6a1bbc885c1d8", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "1eeb0f0193db06", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.com>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
